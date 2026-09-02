ffab3be654be69bb386372826de6ff079da26972
Parth511 <parth1658in@gmail.com>
2026-09-02 10:27:05 +0530

    Add files via upload

---
+++ FOR (26BCP263_6).c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int i,n;
+    printf ("Enter a number to get that total number of even numbers");
+    scanf ("%d",&n);
+    for ( i=1 ; i<=n ; i++)
+    {
+     printf ("%d\n",i*2 );
+    }
+}
