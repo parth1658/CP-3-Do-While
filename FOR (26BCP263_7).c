dc04a2de18da75277d7b81eb90bf087faaccfa6d
Parth511 <parth1658in@gmail.com>
2026-09-02 10:55:13 +0530

    Update FOR (26BCP263_7).c

--- FOR (26BCP263_7).c
+++ FOR (26BCP263_7).c
@@ -2,10 +2,10 @@
 int main()
 {
     int i,n;
-    printf ("Enter a number to get that total number of odd numbers");
+    printf ("Enter a number to get sum of that all numbers");
     scanf ("%d",&n);
     for ( i=1 ; i<=n ; i++)
     {
-     printf ("%d\n",i*2 - 1);
+     printf ("%d\n",i*(i+1)/2);
     }
 }
